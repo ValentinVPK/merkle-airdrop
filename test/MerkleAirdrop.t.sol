699d7466745784b07e812c081e1a11032c869267
ValentinVPK <valentinvpk@abv.bg>
2025-04-06 15:09:18 +0300

    Implement deployment script

--- test/MerkleAirdrop.t.sol
+++ test/MerkleAirdrop.t.sol
@@ -4,8 +4,10 @@ pragma solidity ^0.8.24;
 import {Test, console} from "forge-std/Test.sol";
 import {MerkleAirdrop} from "../src/MerkleAirdrop.sol";
 import {KettlebellToken} from "../src/KettleBellToken.sol";
+import {ZkSyncChainChecker} from "lib/foundry-devops/src/ZkSyncChainChecker.sol";
+import {DeployMerkleAirdrop} from "../script/DeployMerkleAirdrop.s.sol";
 
-contract MerkleAirdropTest is Test {
+contract MerkleAirdropTest is ZkSyncChainChecker, Test {
     MerkleAirdrop public airdrop;
     KettlebellToken public token;
 
@@ -19,10 +21,17 @@ contract MerkleAirdropTest is Test {
     uint256 userPrivateKey;
 
     function setUp() public {
-        token = new KettlebellToken();
-        airdrop = new MerkleAirdrop(ROOT, token);
-        token.mint(token.owner(), AMOUNT_TO_MINT);
-        token.transfer(address(airdrop), AMOUNT_TO_MINT);
+        if (!isZkSyncChain()) {
+            // deploy with script
+            DeployMerkleAirdrop deployer = new DeployMerkleAirdrop();
+            (airdrop, token) = deployer.deployMerkleAirdrop();
+        } else {
+            token = new KettlebellToken();
+            airdrop = new MerkleAirdrop(ROOT, token);
+            token.mint(token.owner(), AMOUNT_TO_MINT);
+            token.transfer(address(airdrop), AMOUNT_TO_MINT);
+        }
+
         (user, userPrivateKey) = makeAddrAndKey("user");
     }
 
