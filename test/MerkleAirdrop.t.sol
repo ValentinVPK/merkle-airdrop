e5107ddeaa96c65337d433e25d5b1a5146664c4c
ValentinVPK <valentinvpk@abv.bg>
2025-04-07 21:15:03 +0300

    Test the claim with the signature

--- test/MerkleAirdrop.t.sol
+++ test/MerkleAirdrop.t.sol
@@ -17,6 +17,7 @@ contract MerkleAirdropTest is ZkSyncChainChecker, Test {
     bytes32 proofOne = bytes32(0x0fd7c981d39bece61f7499702bf59b3114a90e66b51ba2c53abdf7b62986c00a);
     bytes32 proofTwo = bytes32(0xe5ebd1e1b5a5478a944ecab36a9a954ac3b6b8216875f6524caa7a1d87096576);
     bytes32[] public PROOF = [proofOne, proofTwo];
+    address public gasPayer;
     address user;
     uint256 userPrivateKey;
 
@@ -33,13 +34,19 @@ contract MerkleAirdropTest is ZkSyncChainChecker, Test {
         }
 
         (user, userPrivateKey) = makeAddrAndKey("user");
+        gasPayer = makeAddr("gasPayer");
     }
 
     function testUsersCanClaim() public {
         uint256 startingBalance = token.balanceOf(user);
+        bytes32 digest = airdrop.getMessageHash(user, AMOUNT_TO_CLAIM);
 
-        vm.prank(user);
-        airdrop.claim(user, AMOUNT_TO_CLAIM, PROOF);
+        // sign a message
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(userPrivateKey, digest);
+
+        // gasPayer calls claim using the signed message
+        vm.prank(gasPayer);
+        airdrop.claim(user, AMOUNT_TO_CLAIM, PROOF, v, r, s);
 
         uint256 endingBalance = token.balanceOf(user);
         console.log("ending balance: %s", endingBalance);
